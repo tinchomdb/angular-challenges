aedfc8a059e0ba892736a36fb6dc8600bdb34f94
Martin Barroso <79174533+tinchomdb@users.noreply.github.com>
2025-12-29 18:43:01 +0100

    feat: module to Standalone Challenge #31

--- apps/angular/31-module-to-standalone/src/app/app.component.ts
+++ apps/angular/31-module-to-standalone/src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   template: `
     <div class="flex gap-2">
       <button
@@ -25,6 +28,19 @@ import { Component } from '@angular/core';
   host: {
     class: 'flex flex-col p-4 gap-3',
   },
-  standalone: false,
 })
-export class AppComponent {}
+export class AppComponent {
+  /* 
+  the goal is to see how Nx and standalone components work together, and experience the process of decoupling your app with Nx lib and standalone components.
+
+  Finally, standalone components are very simple to understand, but routing/lazy-loaded components can be a bit harder to grasp. This challenge will allow you to manipulate components at different levels of nesting and work with lazy loaded routes.
+
+  After completing this challenge, standalone components will no longer hold any secrets for you.
+
+  Statement
+  The goal of this challenge is to migrate your application from module based components to standalone components.
+
+  Note
+  You can also test the Angular schematic to migrate NgModule to Standalone components. (Since we are using nx, start your command with nx instead of ng) 
+  */
+}
--- apps/angular/31-module-to-standalone/src/app/app.module.ts
+++ apps/angular/31-module-to-standalone/src/app/app.module.ts
@@ -1,11 +1,7 @@
-import { MainShellModule } from '@angular-challenges/module-to-standalone/shell';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-
-@NgModule({
+/* @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, MainShellModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+ */
