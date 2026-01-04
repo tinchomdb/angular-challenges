b0694ece8f5cbd51f6750bb748574b85fc806abf
Martin Barroso <79174533+tinchomdb@users.noreply.github.com>
2026-01-04 15:04:43 +0100

    feat: catchError Challenge #38

--- apps/rxjs/38-catch-error/src/app/app.component.ts
+++ apps/rxjs/38-catch-error/src/app/app.component.ts
@@ -3,29 +3,12 @@ import { HttpClient } from '@angular/common/http';
 import { Component, DestroyRef, OnInit, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormsModule } from '@angular/forms';
-import { Subject, concatMap, map } from 'rxjs';
+import { Subject, catchError, concatMap, map, of } from 'rxjs';
 
 @Component({
   imports: [CommonModule, FormsModule],
   selector: 'app-root',
-  template: `
-    <div class="form-container">
-      <span>
-        possible values: posts, comments, albums, photos, todos, users
-      </span>
-    </div>
-    <form class="form-container" (ngSubmit)="submit$.next()">
-      <input
-        type="text"
-        placeholder="Enter text"
-        [(ngModel)]="input"
-        name="action" />
-      <button>Fetch</button>
-    </form>
-    <div class="response">
-      {{ response | json }}
-    </div>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
@@ -41,7 +24,12 @@ export class AppComponent implements OnInit {
       .pipe(
         map(() => this.input),
         concatMap((value) =>
-          this.http.get(`https://jsonplaceholder.typicode.com/${value}/1`),
+          this.http.get(`https://jsonplaceholder.typicode.com/${value}/1`).pipe(
+            catchError((error) => {
+              console.log(error);
+              return of(null);
+            }),
+          ),
         ),
         takeUntilDestroyed(this.destroyRef),
       )
@@ -57,4 +45,24 @@ export class AppComponent implements OnInit {
         complete: () => console.log('done'),
       });
   }
+
+  /* 
+  Information
+How to Use the Application
+Our application features a form with a text input box and a “Fetch” button. Upon clicking the “Fetch” button, data is retrieved from a free API.
+
+The correct values for a successful response are limited to: posts, comments, albums, photos, todos, and users. Any other values will result in an error response.
+
+Bug
+A bug has been identified in our application. Users are only able to successfully fetch data until an invalid request is sent. Once an error response is received, users are unable to send additional requests.
+
+Learnings
+This application provides an opportunity to understand the correct placement of a catchError operator. If placed incorrectly, the overall subscription will be completed, preventing users from sending more requests. The goal is to preserve the overall subscription by handling error notifications from inner observables appropriately.
+
+Statement
+The goal is to use the catchError operator to handle error management inside your Rxjs stream.
+
+Constraints
+Users should be able to log the value/error each time they click the “Fetch” button.
+  */
 }
